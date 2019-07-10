9cf8827cba3f6a3803b62538b471ba936f0ad00b
Lucas Zacchi <zacchilucasm@gmail.com>
2019-07-10 00:01:14 -0300

    Delete old folders

--- java/atividade_8/src/main/java/org/example/ine5410/MergeSort.java
+++
@@ -1,9 +0,0 @@
-package org.example.ine5410;
-
-import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface MergeSort<T extends Comparable<T>> {
-    @Nonnull ArrayList<T> sort(@Nonnull List<T> list);
-}
--- mutex/atividade_4/exercicio_3/helper.c
+++
@@ -1,8 +0,0 @@
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-void imprimir_resultados(int n, int** results) {
-    for (int i = 0; i < n; ++i) 
-        printf("results[%d]: %d\n", i, *results[i]);
-}
